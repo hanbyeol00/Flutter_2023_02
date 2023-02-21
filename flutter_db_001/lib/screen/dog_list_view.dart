9bc3585a8d03c9e0de5a78546e6d2be428d6bc3f
hanbyeol00 <gksquf6670@gmail.com>
2023-02-21 16:13:59 +0900

    flutter

--- flutter_db_001/lib/screen/dog_list_view.dart
+++ flutter_db_001/lib/screen/dog_list_view.dart
@@ -18,6 +18,9 @@ class DogListView extends StatelessWidget {
         Dog dog = snapshot.data![index];
         return Dismissible(
           key: UniqueKey(),
+          confirmDismiss: (direction) {
+            return confirmDelete(context, dog);
+          },
           onDismissed: (direction) {
             DogDBService().delete(dog.id);
             // setState(() {});
@@ -47,6 +50,53 @@ class DogListView extends StatelessWidget {
               ),
             ],
           )),
+        );
+      },
+    );
+  }
+
+  Future<bool?> confirmDelete(BuildContext context, Dog dog) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          title: Column(
+            children: const [Text("주의!! 데이터 삭제")],
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("${dog.name} : 삭제된 데이터는 복구할 수 없습니다"),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.redAccent,
+              ),
+              onPressed: () {
+                Navigator.pop(context, true);
+              },
+              child: const Text("정말 삭제 할까요?"),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                elevation: 7,
+              ),
+              onPressed: () {
+                Navigator.pop(context, false);
+              },
+              child: const Text(
+                "취소",
+                style: TextStyle(color: Colors.black),
+              ),
+            )
+          ],
         );
       },
     );
